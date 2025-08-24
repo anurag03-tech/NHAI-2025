3a6f3c4da275614539c34c9909b0bb65892d43c5
anurag03-tech <anurag03.tech@gmail.com>
2025-08-24 16:39:26 +0530

    fix bug

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -23,8 +23,18 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use(
   cors({
-    origin: NODE_ENV === "development" ? CLIENT_URL : CLIENT_URL_PROD,
-    credentials: true,
+    origin: function (origin, callback) {
+      // Allow null origin (mobile apps) + your web URLs
+      if (
+        !origin ||
+        [process.env.CLIENT_URL, process.env.CLIENT_URL_PROD].includes(origin)
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // keep cookies for web if needed
   })
 );
 
